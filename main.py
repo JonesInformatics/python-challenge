12475a4415451d6dfd74d35a3374a2987b9eff2c
Liz Jones <52631597+searanch@users.noreply.github.com>
2019-12-13 13:37:52 -0600

    Add files via upload
    finding issue file not found error

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import csv, os
+
+file_to_load = os.path.join( "Resources", "budget_data.csv")
+file_to_output = os.path.join( "analysis", "budget_analysis.txt")
+
+total_months = 0
+pre_revenue = 0
+month_of_change = []
+revenue_change_list = []
+greatest_increase = ["",0]
+greatest_decrease = ["",999999999]
+
+with open(file_to_load) as revenue_data:
+    reader=csv.DictReader(revenue_data)
+
+    for row in reader:
+        total_months = total_months + 1
+        total_revenue = int(row["Profit/Losses"])
+
