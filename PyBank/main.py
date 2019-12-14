b9ff30bb7e17eb6cf70cf9503ca8510c7d946ba4
searanch <52631597+searanch@users.noreply.github.com>
2019-12-14 16:05:31 -0600

    Saturday work

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,21 @@
 
 import csv, os
 
-file_to_load = os.path.join("Resources", "budget_data.csv")
-file_to_output = os.path.join("analysis", "budget_analysis.txt")
+file_to_load = os.path.join( "Resources", "budget_data.csv")
+file_to_output = os.path.join( "analysis", "budget_analysis.txt")
 
+# print(file_to_load)
+# total_revenue = ([1])
+
+
+
+#total_revenue = file_to_load[1]
+
+
+
+
+
+total_revenue = 0
 total_months = 0
 pre_revenue = 0
 month_of_change = []
@@ -12,9 +24,40 @@ greatest_increase = ["",0]
 greatest_decrease = ["",999999999]
 
 with open(file_to_load) as revenue_data:
-    reader=csv.DictReader(revenue_data)
+    reader=csv.reader(revenue_data)
 
     for row in reader:
         total_months = total_months + 1
-        total_revenue = int(row["Profit/Losses"])
+        # total_revenue = int(row["Profit/Losses"])
+
+      # skip reading the header
+      # 
+      #   total_revenue += row[1]
+
+        print(row[1])
+
+    print(total_months)
+
+
+#print total month outside of for loop
+
+
+        
+
+
+
+
+    
+        
+
+        
+        
+
+#         max_age = None
+# oldest_person = None
+# for row in input_file:
+#     age = int(row["age"])
+#     if max_age == None or max_age < age:
+#         max_age = age
+#         oldest_person = row["name"]
 
