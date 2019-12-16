86ab85f5f9f7b8d459217bea6b946c03b950a8a4
searanch <52631597+searanch@users.noreply.github.com>
2019-12-15 19:14:28 -0600

    Months, Total Revenue, Average

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,15 +1,20 @@
+# importing  
 import csv, os
+
+# loading data
 
 file_to_load = os.path.join( "Resources", "budget_data.csv")
 file_to_output = os.path.join( "analysis", "budget_analysis.txt")
 
-
-# # total_revenue = ([1])
-
+# setting variables 
 
 total_revenue = 0
 total_months = 0
 pre_revenue = 0
+diff_revenue = 0
+total_diff_revenue = 0
+current_row_revenue = 0
+ave_change_rev = 0
 month_of_change = []
 revenue_change_list = []
 greatest_increase = ["",0]
@@ -23,18 +28,39 @@ with open(file_to_load) as revenue_data:
 
 # iterating through rows
     for row in reader:
-        total_months = total_months + 1
-        # # # total_revenue = int(row["Profit/Losses"])
 
-      
-        # # # # # total_revenue += (row[1]) DEBUG, int, strg error
-    
-    
-# # # print(row[1])
-        #print(row[1])
-    print("Financial Analysis")
-    print("..................")
-    print(f"Total Months:   {total_months}")
+       # finding total months
+        total_months = total_months + 1
+        
+        # finding total revenue
+        current_row_revenue = int(row[1])
+        total_revenue += current_row_revenue  # DEBUG, int, strg error
+
+        # setting if else statement to account for first row, i ncalculating average monthly change
+        if pre_revenue == 0:
+            diff_revenue = 0
+
+        else:
+            diff_revenue = current_row_revenue - pre_revenue
+
+        total_diff_revenue += diff_revenue
+        pre_revenue = current_row_revenue
+
+ave_change_rev = total_diff_revenue/(total_months -1)
+
+
+print(ave_change_rev)
+print(pre_revenue)
+print(total_months)
+print(total_revenue)
+ 
+print("Financial Analysis")
+print("..................")
+print(f"Total Months:   {total_months}")
+print(f"Total Revenue:  {total_revenue}")
+print(f"Average Monthly Change: {ave_change_rev}")
+
+
 
 
 #print total month outside of for loop
